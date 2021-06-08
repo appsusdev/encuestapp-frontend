e3597e6678222d655b9b8f8508b3221edde80539
Natalia Meneses <nmeneses8@gmail.com>
2021-06-08 14:45:38 -0500

    Interfaces estadisticas

---
+++ src/components/admin/statistics/Statistics.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import clsx from "clsx";
+import { FormattedMessage, useIntl } from "react-intl";
+
+import {
+  Box,
+  Button,
+  Grid,
+  TextField,
+  Paper,
+  Divider,
+  MenuItem,
+} from "@material-ui/core";
+import { useStyles } from "../../../shared/styles/useStyles";
+
+export const Statistics = () => {
+  const classes = useStyles();
+  const intl = useIntl();
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setValue(event.target.value as string);
+  };
+
+  const handleSearch = () => {
+    console.log(value);
+  };
+
+  return (
+    <>
+      <Divider variant="fullWidth" />
+      <Paper square className={classes.paper}>
+        <Box m={2} mb={2}>
+          <Box mb={2}>
+            <Grid container spacing={4}>
+              <Grid item xs={9} className={classes.typography}>
+                <label className="form-text">
+                  <FormattedMessage id="SelectSurvey" />
+                </label>
+                <TextField
+                  className={classes.myTextFieldRoot}
+                  InputLabelProps={{ shrink: false }}
+                  label={`${intl.formatMessage({ id: "InputSelect" })}`}
+                  name="value"
+                  onChange={handleChange}
+                  select
+                  size="small"
+                  value={value}
+                  variant="outlined"
+                >
+                  <MenuItem value={"Encuesta 1"}>Encuesta 1</MenuItem>
+                </TextField>
+              </Grid>
+
+              <Grid item xs={3}>
+                <Button
+                  style={{ marginTop: "27px" }}
+                  size="medium"
+                  className={clsx(classes.btnAction, classes.save)}
+                  onClick={handleSearch}
+                >
+                  <FormattedMessage id="Search" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Paper>
+    </>
+  );
+};
---
+++ src/components/admin/statistics/StatisticsTabs.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useIntl } from "react-intl";
+
+import { AppBar, Tab, Tabs } from "@material-ui/core";
+import { a11yProps, TabPanel, useStyles } from "../../custom/Tabs";
+import { Microdata } from "./Microdata";
+import { Surveyors } from "./Surveyors";
+import { Statistics } from "./Statistics";
+
+export const StatisticsTabs = () => {
+  const classes = useStyles();
+  const intl = useIntl();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          className={classes.tabs}
+          value={value}
+          onChange={handleChange}
+          variant="fullWidth"
+          centered
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "#0A8FDC",
+            },
+          }}
+        >
+          <Tab
+            className={classes.tab}
+            label={`${intl.formatMessage({ id: "Microdata" })}`}
+            {...a11yProps(0)}
+          />
+          <Tab
+            className={classes.tab}
+            label={`${intl.formatMessage({ id: "Surveyors" })}`}
+            {...a11yProps(1)}
+          />
+          <Tab
+            className={classes.tab}
+            label={`${intl.formatMessage({ id: "Statistics" })}`}
+            {...a11yProps(2)}
+          />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Microdata />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Surveyors />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Statistics />
+      </TabPanel>
+    </div>
+  );
+};
