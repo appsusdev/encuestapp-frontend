cdbd35cbd2e72163db5b97a06457059799329ff0
Natalia Meneses <nmeneses8@gmail.com>
2021-08-24 16:20:17 -0500

    Cambio de logo

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,35 +1,31 @@
-import { Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Hidden from '@material-ui/core/Hidden';
-import logo from '../../../assets/images/mi-alcaldia-logo.png'
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Hidden from "@material-ui/core/Hidden";
+import logo from "../../../assets/images/logo-encuestapp-2.png";
 
 const Logo = () => {
-    const useStyles = makeStyles(() => ({
-        logoRoot: {
-            flexDirection: 'row',
-            cursor: 'pointer',
-            position: 'fixed',
-            right: 5,
-        },
-        logo: {
-            height: 50,
-            marginRight: 10,
-            marginTop: 3
-        },
-    }));
-    const classes = useStyles();
-    
-    return (
-        <Box className={classes.logoRoot}>
-            <Hidden >
-                <img
-                    className={classes.logo}
-                    src={logo}
-                    alt='crema-logo'
-                />
-            </Hidden>
-        </Box>
-    );
+  const useStyles = makeStyles(() => ({
+    logoRoot: {
+      flexDirection: "row",
+      cursor: "pointer",
+      position: "fixed",
+      right: 5,
+    },
+    logo: {
+      height: 65,
+      marginRight: 10,
+      marginTop: 3,
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.logoRoot}>
+      <Hidden>
+        <img className={classes.logo} src={logo} alt="crema-logo" />
+      </Hidden>
+    </Box>
+  );
 };
 
 export default Logo;
--- src/pages/auth/LoginScreen.tsx
+++ src/pages/auth/LoginScreen.tsx
@@ -1,61 +1,71 @@
-import { FC } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { useDispatch, useSelector } from 'react-redux';
+import { FC } from "react";
+import { FormattedMessage } from "react-intl";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Box, Card } from '@material-ui/core';
-import { LoginForm } from '../../components/auth/LoginForm';
-import useStylesAuth from '../../components/auth/auth.styles';
-import { Fonts } from '../../shared/constants/AppEnums';
-import { AppState } from '../../redux/reducers/rootReducer';
-import { uiCloseErrorAlert } from '../../redux/actions/uiActions';
-import logo from '../../assets/images/mi-alcaldia-logo.png';
-import { MyAlert } from '../../components/custom/MyAlert';
+import { Box, Card } from "@material-ui/core";
+import { LoginForm } from "../../components/auth/LoginForm";
+import useStylesAuth from "../../components/auth/auth.styles";
+import { Fonts } from "../../shared/constants/AppEnums";
+import { AppState } from "../../redux/reducers/rootReducer";
+import { uiCloseErrorAlert } from "../../redux/actions/uiActions";
+import logo from "../../assets/images/logo-encuestapp-blanco-2.png";
+import { MyAlert } from "../../components/custom/MyAlert";
 
 export const LoginScreen: FC<{}> = () => {
-    const classes = useStylesAuth();
-    const dispatch = useDispatch();
-    const { errorAlert } = useSelector<AppState, AppState['ui']>(state => state.ui);
+  const classes = useStylesAuth();
+  const dispatch = useDispatch();
+  const { errorAlert } = useSelector<AppState, AppState["ui"]>(
+    (state) => state.ui
+  );
 
-    const handleClose = () => {
-        dispatch( uiCloseErrorAlert() );
-    }
+  const handleClose = () => {
+    dispatch(uiCloseErrorAlert());
+  };
 
-    return (
-        <Box className={classes.appAuth}>
+  return (
+    <Box className={classes.appAuth}>
+      <Box
+        flex={1}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Box mb={5} textAlign="center">
+          <img className={classes.imgRoot} src={logo} alt="Logo Encuestapp" />
+        </Box>
 
-            <Box flex={1} display='flex' flexDirection='column' justifyContent='center'>
-                <Box mb={{ xs: 6, md: 8, xl: 18 }} textAlign='center'>
-                    <img
-                        className={classes.imgRoot}
-                        src={logo}
-                        alt='crema-logo'
-                    />
-                </Box>
-
-                <Box
-                    m={1}
-                    display='flex'
-                    flexDirection='column'
-                    justifyContent='center'
-                    alignItems='center'>
-                    <Card className={classes.cardRoot}>
-                        <Box px={{ xs: 6, sm: 10, xl: 15 }}>
-                            <Box
-                                component='h2'
-                                mb={{ xs: 0, xl: 0 }}
-                                color='#495047'
-                                fontWeight={Fonts.REGULAR}
-                                fontSize={{ xs: 24, xl: 26 }}>
-                                <FormattedMessage id='Login' />
-                            </Box>
-                        </Box>
-
-                        <LoginForm />
-                    </Card>
-                </Box>
+        <Box
+          m={1}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Card className={classes.cardRoot}>
+            <Box px={{ xs: 6, sm: 10, xl: 15 }}>
+              <Box
+                component="h2"
+                mb={{ xs: 0, xl: 0 }}
+                color="#495047"
+                fontWeight={Fonts.REGULAR}
+                fontSize={{ xs: 24, xl: 26 }}
+              >
+                <FormattedMessage id="Login" />
+              </Box>
             </Box>
 
-            <MyAlert open={errorAlert} typeAlert="error" message="IncorrectEmailPassword" time={4000} handleClose={handleClose}/>
+            <LoginForm />
+          </Card>
         </Box>
-    )
-}
+      </Box>
+
+      <MyAlert
+        open={errorAlert}
+        typeAlert="error"
+        message="IncorrectEmailPassword"
+        time={4000}
+        handleClose={handleClose}
+      />
+    </Box>
+  );
+};
