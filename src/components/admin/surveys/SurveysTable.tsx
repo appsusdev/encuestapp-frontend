ac9eaa6a41ad41fa54164f8bc6350ae1f06975d5
Natalia Meneses <nmeneses8@gmail.com>
2021-08-31 18:37:40 -0500

    Mensaje no existen encuestas creadas

--- src/components/admin/surveys/SurveysTable.tsx
+++ src/components/admin/surveys/SurveysTable.tsx
@@ -14,12 +14,15 @@ import {
   TableHead,
   ThemeProvider,
   TableBody,
+  Box,
 } from "@material-ui/core";
 import { TablePaginationAct } from "../../custom/TablePaginationAct";
 import { useStyles } from "../../../shared/styles/useStyles";
 import { SurveysBody } from "./SurveysBody";
 import { AppState } from "../../../redux/reducers/rootReducer";
 import { Survey } from "../../../interfaces/Survey";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import { Colors } from "../../../shared/constants/Colors";
 
 const theme = createMuiTheme({
   typography: {
@@ -70,67 +73,82 @@ export const SurveysTable: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="custom pagination table">
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <FormattedMessage id="Name" />{" "}
-              </TableCell>
-              <TableCell align="center">
-                <FormattedMessage id="CreationDate" />{" "}
-              </TableCell>
-              <TableCell align="center">
-                <FormattedMessage id="State" />{" "}
-              </TableCell>
-              <TableCell align="center">
-                <FormattedMessage id="Actions" />{" "}
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {!value.trim()
-              ? (rowsPerPage > 0
-                  ? list.slice(
-                      page * rowsPerPage,
-                      page * rowsPerPage + rowsPerPage
-                    )
-                  : list
-                ).map((survey) => <SurveysBody key={survey.code} {...survey} />)
-              : data &&
-                (rowsPerPage > 0
-                  ? data.slice(
-                      page * rowsPerPage,
-                      page * rowsPerPage + rowsPerPage
-                    )
-                  : data
-                ).map((survey) => (
-                  <SurveysBody key={survey.code} {...survey} />
-                ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 23 * emptyRows }}>
-                <TableCell colSpan={6} />
+        {list.length === 0 ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            style={{ height: 200 }}
+            className={classes.title}
+          >
+            <InfoOutlinedIcon style={{ fontSize: 30, color: Colors.ACCENT }} />
+            <FormattedMessage id={"NoCreatedSurveys"} />
+          </Box>
+        ) : (
+          <Table className={classes.table} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <FormattedMessage id="Name" />{" "}
+                </TableCell>
+                <TableCell align="center">
+                  <FormattedMessage id="CreationDate" />{" "}
+                </TableCell>
+                <TableCell align="center">
+                  <FormattedMessage id="State" />{" "}
+                </TableCell>
+                <TableCell align="center">
+                  <FormattedMessage id="Actions" />{" "}
+                </TableCell>
               </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5]}
-                colSpan={6}
-                count={count}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: { "aria-label": "rows per page" },
-                  native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationAct}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {!value.trim()
+                ? (rowsPerPage > 0
+                    ? list.slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                    : list
+                  ).map((survey) => (
+                    <SurveysBody key={survey.code} {...survey} />
+                  ))
+                : data &&
+                  (rowsPerPage > 0
+                    ? data.slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                    : data
+                  ).map((survey) => (
+                    <SurveysBody key={survey.code} {...survey} />
+                  ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 23 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5]}
+                  colSpan={6}
+                  count={count}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: { "aria-label": "rows per page" },
+                    native: true,
+                  }}
+                  onChangePage={handleChangePage}
+                  onChangeRowsPerPage={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationAct}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        )}
       </TableContainer>
     </ThemeProvider>
   );
