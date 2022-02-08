b96ce2338793000c2fed75d32935f2c6f8464fc8
Natalia Meneses <nmeneses8@gmail.com>
2022-02-08 18:12:34 -0500

    Arreglos en vista de pdfs

--- src/components/admin/home/PDFSurveys.tsx
+++ src/components/admin/home/PDFSurveys.tsx
@@ -210,35 +210,45 @@ export const PDFSurveys = (props: Props) => {
             {chapter.questions.map((question, index) => (
               <Box m={1} mb={1} key={question.id}>
                 <Grid container>
-                  <Grid item xs={1}>
-                    {question.directedTo === "PreguntasIndividual" ? (
-                      <PersonIcon
-                        fontSize="small"
-                        style={{ marginTop: "2px" }}
-                      />
-                    ) : (
-                      <HomeIcon fontSize="small" style={{ marginTop: "2px" }} />
-                    )}
+                  <Grid item xs={12}>
+                    <Box display={"flex"}>
+                      {question.directedTo === "PreguntasIndividual" ? (
+                        <PersonIcon
+                          fontSize="small"
+                          style={{ marginTop: "2px" }}
+                        />
+                      ) : (
+                        <HomeIcon
+                          fontSize="small"
+                          style={{ marginTop: "2px" }}
+                        />
+                      )}
+                      <Box ml={1}>
+                        {chapter.number}.{index + 1} {question.question}
+                      </Box>
+                    </Box>
                   </Grid>
 
-                  <Grid item xs={11} style={{ marginLeft: "-10px" }}>
-                    {chapter.number}.{index + 1} {question.question}
-                  </Grid>
                   {question.answers.map(
                     (answer: ISurveyAnswers, indexAnswer: number) => (
                       <Fragment key={indexAnswer}>
-                        <Grid item xs={12}>
-                          <p
-                            style={{ marginTop: "10px" }}
-                            className={classes.capitalize}
-                          >
-                            {getUsername(answer.citizen)}:
-                          </p>
-                        </Grid>
+                        {question.directedTo === "PreguntasIndividual" &&
+                          getUsername(answer.citizen) !== "" && (
+                            <Grid item xs={12}>
+                              <p
+                                style={{
+                                  marginTop: "10px",
+                                  fontWeight: "bold",
+                                }}
+                                className={classes.capitalize}
+                              >
+                                {getUsername(answer.citizen)}:
+                              </p>
+                            </Grid>
+                          )}
                         <Grid
                           item
                           xs={
-                            question.type === TypeQuestion.TEXT_AREA ||
                             question.type === TypeQuestion.PICTURE ||
                             question.type === TypeQuestion.FILE ||
                             question.type === TypeQuestion.GEOLOCATION ||
@@ -246,7 +256,7 @@ export const PDFSurveys = (props: Props) => {
                             question.type === TypeQuestion.CHECK ||
                             question.type === TypeQuestion.RADIO
                               ? 12
-                              : 8
+                              : 6
                           }
                         >
                           {(question.type === TypeQuestion.TEXT_INPUT ||
@@ -282,6 +292,7 @@ export const PDFSurveys = (props: Props) => {
                                 answer.respuesta &&
                                 convertDateDash(answer.respuesta.value)
                               }
+                              disabled
                             />
                           )}
 
@@ -318,7 +329,7 @@ export const PDFSurveys = (props: Props) => {
                                     justifyContent={"space-between"}
                                     width={"100%"}
                                   >
-                                    <Box width={"45%"}>
+                                    <Box width={"51%"}>
                                       <TextField
                                         name="input"
                                         variant="outlined"
@@ -332,13 +343,15 @@ export const PDFSurveys = (props: Props) => {
                                         }
                                       />
                                     </Box>
-                                    <Box width={"50%"}>
+                                    <Box width={"49%"} ml={2}>
                                       {option.description && (
                                         <Box
                                           display={"flex"}
                                           justifyContent={"space-between"}
                                         >
-                                          <p>{option.textDescription}:</p>
+                                          <Box width={1 / 2} mt={2}>
+                                            <p>{option.textDescription}:</p>
+                                          </Box>
 
                                           <TextField
                                             name="input"
@@ -370,7 +383,7 @@ export const PDFSurveys = (props: Props) => {
                                       justifyContent={"space-between"}
                                       width={"100%"}
                                     >
-                                      <Box width={"45%"}>
+                                      <Box width={"51%"}>
                                         <TextField
                                           name="input"
                                           variant="outlined"
@@ -384,13 +397,15 @@ export const PDFSurveys = (props: Props) => {
                                         />
                                       </Box>
 
-                                      <Box width={"50%"}>
+                                      <Box width={"49%"} ml={2}>
                                         {option.description && (
                                           <Box
                                             display={"flex"}
                                             justifyContent={"space-between"}
                                           >
-                                            <p>{option.textDescription}:</p>
+                                            <Box width={1 / 2} mt={2}>
+                                              <p>{option.textDescription}:</p>
+                                            </Box>
 
                                             <TextField
                                               name="input"
